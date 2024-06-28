ac00df8868fddf9fca61c3e7ecd2bae4d35aaf7a
Deathspike <meister.deathspike@outlook.com>
2024-06-28 16:12:56 +0200

    Add `adler32` block hash

---
+++ Difflytic/Hash/Adler32/Adler32BlockHash.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Difflytic.Hash.Adler32
+{
+    public sealed class Adler32BlockHash : IBlockHash
+    {
+        private const uint Mod = 65521;
+        private uint _a = 1;
+        private uint _b;
+
+        #region Methods
+
+        private uint Digest()
+        {
+            var hash = (_b << 16) | _a;
+            _a = 1;
+            _b = 0;
+            return hash;
+        }
+
+        #endregion
+
+        #region Implementation of IBlockHash
+
+        public uint AddAndDigest(Span<byte> buffer)
+        {
+            foreach (var value in buffer)
+            {
+                _a += value;
+                _b += _a;
+
+                while (_a >= Mod) _a -= Mod;
+                while (_b >= Mod) _b -= Mod;
+            }
+
+            return Digest();
+        }
+
+        #endregion
+    }
+}
---
+++ Difflytic/Hash/IBlockHash.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Difflytic.Hash
+{
+    public interface IBlockHash
+    {
+        #region Methods
+
+        uint AddAndDigest(Span<byte> buffer);
+
+        #endregion
+    }
+}
