bb9a81e78f5beea1586279783895efe21a7eea7b
Deathspike <meister.deathspike@outlook.com>
2024-07-12 17:58:07 +0200

    Optimize `adler32` block hash

--- Difflytic/Hash/Adler32/Adler32BlockHash.cs
+++ Difflytic/Hash/Adler32/Adler32BlockHash.cs
@@ -4,41 +4,23 @@ namespace Difflytic.Hash.Adler32
 {
     public sealed class Adler32BlockHash : IBlockHash
     {
-        private uint _a;
-        private uint _b;
-
-        #region Constructors
-
-        public Adler32BlockHash()
-        {
-            _a = 1;
-        }
-
-        #endregion
-
-        #region Methods
-
-        private uint Digest()
-        {
-            var hash = (_b << 16) | _a;
-            _a = 1;
-            _b = 0;
-            return hash;
-        }
-
-        #endregion
-
         #region Implementation of IBlockHash
 
         public uint AddAndDigest(Span<byte> buffer)
         {
+            var a = 1u;
+            var b = 0u;
+
             foreach (var value in buffer)
             {
-                _a = (_a + value) % Adler32.Mod;
-                _b = (_b + _a) % Adler32.Mod;
+                a += value;
+                b += a;
+
+                if (a >= Adler32.Mod) a -= Adler32.Mod;
+                if (b >= Adler32.Mod) b -= Adler32.Mod;
             }
 
-            return Digest();
+            return (b << 16) | a;
         }
 
         #endregion
